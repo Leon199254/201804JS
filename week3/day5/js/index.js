1d9a027d02e3e94f3f373317a18ea313b7f32aa4
zfpx <1323682239@qq.com>
2018-06-10 13:03:02 +0800

    m

---
+++ week3/day5/js/index.js
@@ -0,0 +1,93 @@
+//第一步 拿JSON数据 把数据放到页面上
+
+var data = null;
+var xhr = new XMLHttpRequest();
+xhr.open('get','./data.json',false);
+xhr.onreadystatechange = function () {
+    if(xhr.readyState == 4 && /^2(\d){2}$/.test(xhr.status)){
+        data = utils.toJson(xhr.responseText)
+    }
+};
+xhr.send(null);
+console.log(data);
+
+var uls = document.getElementsByClassName('ul');
+
+//1、在这里我们先说好了挨个给ul塞数据
+/*
+data.forEach((item,index)=>{
+    let {src,title,height} = item;
+    let str = ` <li style="height:${height}">
+                    <img src="${src}" alt="">
+                    <p>${title}</p>
+                </li>`;
+    var n = index%4;//n = [0|1|2|3]  对应uls的索引
+    uls[n].innerHTML += str;
+});*/
+
+data.forEach((item,index)=>{
+    let {src,title,height} = item;
+    let str = ` <li style="height:${height}">
+                    <img src="./images/timg.gif" trueSrc="${src}" alt="">
+                    <p>${title}</p>
+                </li>`;
+    let minUl = getMinUl();
+    minUl.innerHTML += str;
+});
+
+//2、思路是先找高度最小的ul塞数据；
+// 拿高度 用clientHeight
+//拿到高度之后找到高度最小的ul
+
+
+//目的是找到最小高度的ul
+function getMinUl() {
+    let ulAry = [...uls];
+    let sortAry = ulAry.sort((a,b)=>{
+        return a.clientHeight - b.clientHeight
+    });
+    return sortAry[0];
+}
+
+let imgs = document.getElementsByTagName('img');
+//这个for是为了给每一个图片加一个自定义属性 用来存储该图片到body的距离
+for(let i=0; i< imgs.length ; i++){
+    // let t = utils.offset(imgs[i]).top+imgs[i].clientHeight;
+    let t = utils.offset(imgs[i]).top + 50;
+    imgs[i].imgTop = t;
+}
+
+//这个函数是为了对每一个img都做一次图片懒加载的处理
+function loadAll() {
+    for(let i = 0; i< imgs.length; i++){
+        loadImg(imgs[i]);
+    }
+}
+
+loadAll()//在这执行是为了让当前屏的图片显示出来
+
+//这个函数是为了在屏幕滚动时执行loadAll函数
+window.onscroll = function () {
+    loadAll();
+};
+
+
+function loadImg(img){
+    if(img.loaded){
+        return;
+    };
+    var tempTop = utils.win('scrollTop') + utils.win('clientHeight');
+    img.style.opacity = 0;
+    if(img.imgTop <= tempTop){
+        var trueSrc = img.getAttribute('trueSrc');
+        var tempImg = document.createElement('img');
+        tempImg.src = trueSrc;
+        tempImg.onload = function () {
+            img.src = trueSrc;
+            img.loaded = true;
+            img.style.opacity = 1;
+        };
+        tempImg = null;
+    }
+}
+
---
+++ week3/day5/js/utils.js
@@ -0,0 +1,85 @@
+var utils = (function () {
+    function getCss(ele,attr) {
+        var val = null;
+        try{
+            val = window.getComputedStyle(ele)[attr];
+        }catch (e) {
+            if(attr == 'opacity'){
+                val = ele.currentStyle.filter;
+                val = /\d+/.exec(val)[0]/100;
+            }else {
+                val = ele.currentStyle[attr];
+            }
+        }
+        var tempVal = parseFloat(val);
+        var reg = /^[+-]?(\d+)(\.\d+)?(px|pt|rem)?$/;
+        val = reg.test(val) ? parseFloat(val) : val;
+        return val;
+    }
+    function setCss(ele,attr,value) {
+        var reg = /^(width|height|fontSize|(margin|padding)?(left|right|top|bottom)?)$/i;
+        if(reg.test(attr)){
+            ele.style[attr] = parseFloat(value) + 'px';
+        }else {
+            ele.style[attr] = value;
+        }
+    }
+    function setGroupCss(ele,obj) {
+        if({}.toString.call(obj) == '[object Object]'){
+            for(var k in obj){
+                setCss(ele,k,obj[k])
+            }
+        }
+    }
+    function css(...arg) {
+        if(arg.length == 3){
+            setCss(arg[0],arg[1],arg[2])
+        }else if(arg.length == 2 && arg[1] instanceof Object){
+            setGroupCss(arg[0],arg[1])
+        }else {
+            return getCss(arg[0],arg[1]);
+        }
+    }
+    function offset(ele) {
+        var l = ele.offsetLeft;
+        var t = ele.offsetTop;
+        var p = ele.offsetParent;
+        while (p.tagName.toLowerCase() != 'body'){
+            l += p.offsetLeft+p.clientLeft;
+            t += p.offsetTop + p.clientTop;
+            p = p.offsetParent;
+        }
+        return {
+            // l,t
+            left:l,
+            top:t
+        }
+    }
+    function toArray(likeAry) {
+        var ary = [];
+        try{
+            ary = [].slice.call(likeAry);
+        }catch (e) {
+            for(var i = 0; i< likeAry.length; i++){
+                ary.push(likeAry[i]);
+            }
+        }
+        return ary;
+    }
+    function toJson(jsonStr) {
+        var obj = null;
+        try{
+            obj = JSON.parse(jsonStr)
+        }catch (e) {
+            obj = eval("("+jsonStr+")");
+        }
+        return obj;
+    }
+    function win(attr) {
+        return document.documentElement[attr] || document.body[attr]
+    }
+    return{
+        // getCss:getCss  === getCss
+        getCss,setCss,setGroupCss,css,offset,toArray,toJson,win
+    }
+})();
